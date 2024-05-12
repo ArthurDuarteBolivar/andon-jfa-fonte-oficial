171c53fc1c00150855e985f5cfad9e8db286607f
arthur77br <bolivarartur77@gmail.com>
2024-05-12 09:45:09 -0300

    justificativa adicionado

--- spring-boot-api-nodemcu-jwt-token/src/main/java/com/api/nodemcu/model/ControleRealizadoModel.java
+++ spring-boot-api-nodemcu-jwt-token/src/main/java/com/api/nodemcu/model/ControleRealizadoModel.java
@@ -1,2 +1,36 @@
-package com.api.nodemcu.model;public class ControleRealizadoModel {
+package com.api.nodemcu.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Entity
+@Data
+@Table(name = "realizado")
+public class ControleRealizadoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private Integer imposto;
+
+    private Integer realizado;
+
+    private Integer realizadoHora;
+    private String justificativa;
+
+    private Date data;
+
+    @PrePersist
+    protected void prePersist() {
+        if (this.data == null) {
+            TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+            data = new Date();
+        }
+    }
+
 }
